a1d16a9fc19b6191d9c2eaabc1c04ed2d2fdb7dc
Maria Taysmar <taysmar@outlook.com>
2022-10-20 12:58:15 -0300

    estudo para desenvolvimento do filtro de pesquisa

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputSearch = document.querySelector("#search")
+const navMenu = document.querySelector("[data-type='nav-menu']")
+
+inputSearch.addEventListener("input", function(){
+    const str = this.value;
+    if(str){
+        filterData(str)
+    } else {
+        showAllItems()
+    }
+})
+
+function showAllItems() {
+    
+}
