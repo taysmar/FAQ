738f953b46ad990716a28c92e0bab17ba6d53c7a
Maria Anulino <taysmar@cda.lan>
2022-10-28 11:56:29 -0300

    anotações e testes

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const inputSearch = document.querySelector("#search")
-const navMenu = document.querySelector("[data-type='nav-menu']")
+// http://localhost:1337/api/perguntas-e-respostas
 
-inputSearch.addEventListener("input", function(){
-    const str = this.value;
-    if(str){
-        filterData(str)
-    } else {
-        showAllItems()
-    }
-})
+// const inputSearch = document.querySelector("#search")
+// const navMenu = document.querySelector("[data-type='nav-menu']")
 
-function showAllItems() {
+// inputSearch.addEventListener("input", function(){
+//     const str = this.value;
+//     if(str){
+//         filterData(str)
+//     } else {
+//         showAllItems()
+//     }
+// })
+
+// function showAllItems() {
     
+// }
+
+const fetchData = () => {
+    const listOfQuestionsElement = document.getElementById("listofQuestions")
+    console.log(listOfQuestionsElement)
 }
+
+
+
+//Token-api : 5b5bd46098ebce69dfa6d1ecd04b57c2a47272101fe5cc22416eb3689cd02c0f78c7f996bdeb40fd882a9945edb13598a7c818bf5194c6b6398061137827ce8d18112f87f05d73a31abc66284297a23e6839e227d78d5dc0d0a1618085b3aa866643e47650fbf2bb3113e3dd244b5758a2ca85a4f7d421992cb34c4a0c61a849
