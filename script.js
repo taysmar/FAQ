f157a3089078821869d709f5e7884066502f80c0
Maria Taysmar <taysmar@outlook.com>
2022-11-01 11:18:39 -0300

    mudanças no scrip

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// http://localhost:1337/api/perguntas-e-respostas
+//Token-api : 5b5bd46098ebce69dfa6d1ecd04b57c2a47272101fe5cc22416eb3689cd02c0f78c7f996bdeb40fd882a9945edb13598a7c818bf5194c6b6398061137827ce8d18112f87f05d73a31abc66284297a23e6839e227d78d5dc0d0a1618085b3aa866643e47650fbf2bb3113e3dd244b5758a2ca85a4f7d421992cb34c4a0c61a849
 
-// const inputSearch = document.querySelector("#search")
-// const navMenu = document.querySelector("[data-type='nav-menu']")
-
-// inputSearch.addEventListener("input", function(){
-//     const str = this.value;
-//     if(str){
-//         filterData(str)
-//     } else {
-//         showAllItems()
-//     }
-// })
-
-// function showAllItems() {
-    
-// }
-
-const fetchData = () => {
-    const listOfQuestionsElement = document.getElementById("listofQuestions")
-    console.log(listOfQuestionsElement)
+function getUrl(url){
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
 }
 
+function newQuestion(question){
 
+}
 
-//Token-api : 5b5bd46098ebce69dfa6d1ecd04b57c2a47272101fe5cc22416eb3689cd02c0f78c7f996bdeb40fd882a9945edb13598a7c818bf5194c6b6398061137827ce8d18112f87f05d73a31abc66284297a23e6839e227d78d5dc0d0a1618085b3aa866643e47650fbf2bb3113e3dd244b5758a2ca85a4f7d421992cb34c4a0c61a849
+function main() { 
+    console.log(getUrl("http://localhost:1337/admin"))
+}
+main
+
+let answers=document.querySelectorAll(".accordion");
+answers.forEach((event)=>{
+ event.addEventListener('click',()=>{
+  if(event.classList.contains("active")){
+   event.classList.remove("active");
+  }
+  else{
+   event.classList.add("active");
+  }
+ })
+})
